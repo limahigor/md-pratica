48d683211c6ee5d916bd7e2347f062a67ddd6894
Higor Lima <limahigor.g@gmail.com>
2022-05-22 17:34:44 -0300

    md pratica

---
+++ md_module.h
@@ -0,0 +1,11 @@
+#ifndef MD_MODULE_H
+#define MD_MODULE_H
+
+int mdc_euclides(unsigned int x, unsigned int y);
+unsigned int isprime(unsigned long int num);
+void maior_primo(int intervalo);
+
+//funcoes pra testes
+void mdc_mmc(int num1, int num2);
+
+#endif
