a76ddf36b5986094178e14f93ca80d439e959c04
Higor Lima <limahigor.g@gmail.com>
2022-05-23 04:17:12 -0300

    delete some files

--- comb_linear.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMB_LINEAR_MODULE_H
-#define COMB_LINEAR_MODULE_H
-
-int* comb_linear_mdc(int x, int y);
-int comb_linear_generica(int x, int y, int z);
-int inverso(int x, int y);
-
-#endif
